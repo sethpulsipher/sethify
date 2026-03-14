a00b1e06b482a5fb392d1c14de1a19aba8527444
Seth Pulsipher <sethpulsipher@gmail.com>
2026-03-13 23:41:35 -0600

    favicon gen init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Sethify" />
+      </head>
       <body
         suppressHydrationWarning
         className={`${dmSerif.variable} ${dmSans.variable} antialiased min-h-screen flex flex-col bg-background text-foreground font-body`}
